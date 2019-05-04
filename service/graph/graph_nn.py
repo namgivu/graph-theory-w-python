df72a60777af63bb9a65b4e2e87467ebea50420a
namgivu <namgivu@gmail.com>
2019-05-04 23:47:56 +0700

    find path from n1 to n2

--- service/graph/graph_nn.py
+++ service/graph/graph_nn.py
@@ -1,10 +1,10 @@
 N      = 0 # N aka number of node
-
-array1 = [0] * N
-A      = array1 * N # N*N matrix representing the graph/node-network
+A      = [[0 for __ in range(N)] for _ in range(N)] # N*N matrix representing the graph/node-network
+G      = { 'some-node': {'neighbor0', 'neighbor1', 'neighbor2'} }
 
 node_names = dict()
 node_ids   = dict()
+
 
 def load(init_data:dict):
     """
@@ -35,7 +35,7 @@ def load(init_data:dict):
         node_ids[node_name] = i
     #endregion
 
-    # load the graph
+    # load the graph as A[i,j]
     global N; N = len(node_names)
     global A; A = [ [0 for __ in range(N)] for _ in range(N) ]
     for node_name in init_data:
@@ -45,3 +45,56 @@ def load(init_data:dict):
             j = node_ids[nnn]
             A[i][j] = 1
             A[j][i] = 1
+
+    # load the graph as G{ node -> neighbor_set }
+    global G; G = dict()
+    for node_name in init_data:
+        i = node_ids[node_name]
+        G[i] = set()
+
+        neighbor_node_names = init_data[node_name]
+        for nnn in neighbor_node_names:
+            j = node_ids[nnn]
+            G[i].add(j)
+
+
+def find_path(from_node_name, to_node_name):
+    global N, A, G, node_names, node_ids
+
+    if from_node_name not in node_ids.keys(): return []
+    if to_node_name   not in node_ids.keys(): return []
+
+    node_f = node_ids[from_node_name] # node_f aka from node
+    node_t = node_ids[to_node_name]   # node_t aka to node aka target node
+
+    s1 = set(); s1.add(node_f)
+    s2 = set()
+    tr = {}; tr[node_f]=None # tr aka trace back; tr[i] == node before node :i
+    while len(s1)>0:
+        #region find nodes connected with s1, save to s2
+        for i in s1:
+            #region find nodes j connected to i
+            for j in G[i]:
+                if tr.get(j) is None:
+                    tr[j] = i
+                    s2.add(j)
+            #endregion
+        #endregion
+
+        if tr.get(node_t): break # we found the target node, seek no more
+
+        # continue with nodes found in s2
+        s1 = s2
+        s2 = set()
+
+    if tr.get(node_t) is None: return [] # we cannot reach the target node
+
+    p = [] # p aka path
+    i = node_t
+    while i!=node_f:
+        p.append(i)
+        i = tr[i]
+    p.append(i)
+
+    p = p[::-1]
+    return p
--- test/service/graph/test_graph_nn.py
+++ test/service/graph/test_graph_nn.py
@@ -1,24 +1,25 @@
 import unittest
 import service.graph.graph_nn as GraphNN; G = GraphNN # G defined as short alias
 
+GRAPH_FIXTURE = {
+    'a': ['d'],
+    'b': ['c'],
+    'c': ['b', 'c', 'd', 'e'],
+    'd': ['a', 'c'],
+    'e': ['c'],
+    'f': [],
+}
+
 
 class Test(unittest.TestCase):
 
-    def test_abcdef(self):
-        d = {
-            'a': ['d'],
-            'b': ['c'],
-            'c': ['b', 'c', 'd', 'e'],
-            'd': ['a', 'c'],
-            'e': ['c'],
-            'f': [],
-        }
-        G.load(d)
+    def test_load_abcdef(self):
+        G.load(init_data=GRAPH_FIXTURE)
         assert set(G.node_names.keys()  ) == { 0,   1,   2,   3,   4,   5,  }
         assert set(G.node_names.values()) == {'a', 'b', 'c', 'd', 'e', 'f', }
 
 
-    def test_012345(self):
+    def test_load_012345(self):
         d = {
             0 : [3],
             1 : [2],
@@ -31,3 +32,10 @@ class Test(unittest.TestCase):
         assert set(G.node_names.keys()  ) == { 0,   1,   2,   3,   4,   5, }
         assert set(G.node_names.values()) == { 0,   1,   2,   3,   4,   5, }
 
+
+    def test_find_path(self):
+        G.load(init_data=GRAPH_FIXTURE)
+        # f='a'; t='b'; ep=['a', 'd', 'c', 'b']; p = G.find_path(f, t); p = [G.node_names[i] for i in p]; assert p == ep
+        # f='a'; t='f'; ep=[];                   p = G.find_path(f, t); p = [G.node_names[i] for i in p]; assert p == ep
+        # f='c'; t='c'; ep=['c'];                p = G.find_path(f, t); p = [G.node_names[i] for i in p]; assert p == ep
+        f='a'; t='a'; ep=['a'];                p = G.find_path(f, t); p = [G.node_names[i] for i in p]; assert p == ep
